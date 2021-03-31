ddfe23c419e5d4f3172642fdf271ae5f55595c12
David LeBauer <dlebauer@arizona.edu>
2021-03-30 17:47:39 -0700

    added some multivariate / mixed-effect models

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -1,22 +1,32 @@
-library(forecast)
+library(dplyr)
 library(tidyr)
+library(readr)
+library(ggplot2)
 
-x <- readr::read_csv('../USA organic agriculture state data in farms_landarea_sales value.csv', 
+x <- readr::read_csv('USA organic agriculture state data in farms_landarea_sales value.csv', 
                      col_types = cols(
                        year = col_integer(),
                        state = col_character(),
                        farm_number = col_number(),
                        farm_ha = col_number(),
                        sales = col_number()
-                     )) 
-  
+                     )) %>% 
+  drop_na()
 
-library(ggplot2)
+
+# drop states w/ < 2 years
+
+x2 <- x %>% group_by(state) %>% 
+  summarise(n = n()) %>% 
+  filter(n < 3) 
+
+x3 <- x %>% filter(!state %in% x2$state)
 
 y <- x %>% 
-  mutate(sales_k = sales/1000,
+  mutate(farm_knumber = farm_number/1000,
          farm_kha = farm_ha/1000,
          farm_msales = sales/1000000) %>% 
+  select(!farm_number:sales) %>% 
   tidyr::pivot_longer(cols = farm_number:sales, names_to = 'metric', values_to = 'value')
 
 ggplot(data = y, aes(year, value, color = state)) + 
@@ -43,3 +53,32 @@ ggplot(data = totals, aes(year, us_50)) +
   geom_line(aes(y = us_25), alpha = 0.5)+
   geom_line(aes(y = us_75), alpha = 0.5)+
   facet_wrap(~metric, scales = 'free_y', ncol = 1)
+
+
+### Modeling
+
+library(lme4)
+
+mod_lm <- lm(cbind(farm_number, farm_ha, sales) ~ state + year, data = x3)
+
+
+
+m <- lmer(cbind(farm_number, farm_ha, sales) ~ year * state + (1 + year | state),
+          data = x3)
+
+# https://m-clark.github.io/mixed-models-with-R/bayesian.html
+library(brms)
+## full multivariate (need to drop states w/o all three years)
+m <- brm(mvbind(farm_number, farm_ha, sales) ~ year * state + (1 + year | state),
+         data = x3)
+
+
+library(rstanarm)
+mod_stanlmer <- stan_lmer(cbind(farm_number, farm_ha, sales) ~ year * state + (1 + year | state),
+                          data = x3, na.action = 'na.omit')
+## univariate
+mod_stanlmer1 <- stan_lmer(farm_ha ~ year * state + (1 + year | state),
+               data = x3)
+
+
+
