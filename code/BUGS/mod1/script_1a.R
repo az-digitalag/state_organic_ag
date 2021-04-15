8c821656b6c2776f0422e24d0ec74f1d60026163
Jessica Guo <jessicaguo@github.com>
2021-04-14 17:09:43 -0700

    Separate scripts to describe statistical model and predict future farm characteristics

--- code/BUGS/mod1/script_1a.R
+++ code/BUGS/mod1/script_1a.R
@@ -11,6 +11,7 @@ library(ggplot2)
 library(R2OpenBUGS)
 library(postjags)
 library(mcmcplots)
+library(cowplot)
 
 x <- readr::read_csv('../../../raw_data/USA organic agriculture state data in farms_landarea_sales value.csv', 
                      col_types = cols(
@@ -22,7 +23,7 @@ x <- readr::read_csv('../../../raw_data/USA organic agriculture state data in fa
                      ))
 dat <- x %>% 
   mutate(farm_knumber = farm_number/1000,
-         farm_kha = farm_ha/1000,
+         farm_5ha = farm_ha/100000,
          farm_msales = sales/1000000,
          year = year - 2000,
          stateID = as.numeric(as.factor(state))) %>% # "center" by using years since 2000
@@ -58,6 +59,7 @@ inits <- function(){
   )
 }
 initslist <- list(inits(), inits(), inits())
+load("inits/inits_1a.Rdata")
 
 # Compile and adapt BUGS model
 start <- proc.time()
@@ -72,13 +74,18 @@ print((end - start)/60)
 
 #change to coda object if codaPkg=T
 coda_out <- read.bugs(model)
+save(coda_out, file = "coda/coda_out_1a.Rdata")
+
+#view chains
+mcmcplot(coda_out, parms = c("Dsum", "B"))
+mcmcplot(coda_out, parms = c("Estar"))
 
 #extract final iteration, save initials for reuse
 newinits <- initfind(coda_out, OpenBUGS = TRUE)
 saved.state <- removevars(initsin = newinits, 
                           variables=c(2:5))
 #dir.create("inits")
-save(saved.state, file = "inits_1a.Rdata")
+save(saved.state, file = "inits/inits_1a.Rdata")
 
 round(apply(coda_out[[1]][,7:9],2,mean), 3)
 round(apply(coda_out[[2]][,7:9],2,mean), 3)
@@ -120,7 +127,217 @@ caterplot(coda_out, regex=c("Estar\\[\\d{1,2},1\\]", perl=T), reorder=F)
 caterplot(coda_out, regex=c("Estar\\[\\d{1,2},2\\]", perl=T), reorder=F)
 caterplot(coda_out, regex=c("Estar\\[\\d{1,2},3\\]", perl=T), reorder=F)
 
-#view chains
-mcmcplot(coda_out, parms = c("Dsum", "B"))
-mcmcplot(coda_out, parms = c("Estar"))
 
+#summarizing chains
+sum_out<-coda.fast(chains=3, burn.in=0, thin=1, coda=coda_out)
+sum_out$var <- row.names(sum_out)
+sum_out$sig<-ifelse(sum_out$pc2.5*sum_out$pc97.5 > 0, TRUE, FALSE)
+
+## Plots
+labs <- c("Number (1e3)", "Area (1e5 acres)", "Sales (1e6 $)")
+
+### Intercepts
+dat_B1 <- sum_out[grep("B\\[1", row.names(sum_out)),]
+sig_B1 <- subset(dat_B1, sig == TRUE)
+fig_intercept<-ggplot() +
+  geom_pointrange(data = dat_B1, aes(x = var, y = mean, 
+                                     ymin = pc2.5, ymax = pc97.5), 
+                  size = 0.5) +
+  geom_hline(yintercept=0, col = "red") +
+  scale_y_continuous("Value in 2000")+ 
+  scale_x_discrete(labels = labs) +
+  theme_bw(base_size = 12)+
+  theme(panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.title.y = element_blank()) +
+  coord_flip()
+print(fig_intercept)
+
+### Slopes
+dat_B2 <- sum_out[grep("B\\[2", row.names(sum_out)),]
+sig_B2 <- subset(dat_B2, sig == TRUE)
+fig_slope<-ggplot() +
+  geom_pointrange(data = dat_B2, aes(x = var, y = mean, 
+                                     ymin = pc2.5, ymax = pc97.5), 
+                  size = 0.5) +
+  # geom_point(data = sig_B2, aes(x = var, y = (sig*(max(sub1[,4])+1))), 
+  # col = "red", shape = 8, size = 2)+
+  geom_hline(yintercept=0, col = "red") +
+  scale_y_continuous(expression(paste("Rate of change (", yr^-1, ")")))+ 
+  scale_x_discrete(labels = labs) +
+  theme_bw(base_size = 12)+
+  theme(panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.title.y = element_blank()) +
+  coord_flip()
+print(fig_slope)
+
+jpeg(filename = "figs/fig_B.jpg", height = 4, width = 8, units = "in", 
+     res = 600)
+plot_grid(fig_intercept, fig_slope, ncol = 2)
+dev.off()
+
+## Random effects
+dat_RE1 <- sum_out[grep("Estar\\[[0-9]{1,2}\\,1", row.names(sum_out)),]
+dat_RE1$state <- unique(dat$state)
+fig_RE1<-ggplot() +
+  geom_pointrange(data = dat_RE1, aes(x = state, y = mean, 
+                                     ymin = pc2.5, ymax = pc97.5), 
+                  size = 0.25) +
+  geom_hline(yintercept = 0, col = "red") +
+  scale_y_continuous("RE (number, 1e3)")+ 
+  scale_x_discrete() +
+  theme_bw(base_size = 12)+
+  theme(panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.title.y = element_blank()) +
+  coord_flip()
+print(fig_RE1)
+
+dat_RE2 <- sum_out[grep("Estar\\[[0-9]{1,2}\\,2", row.names(sum_out)),]
+dat_RE2$state <- unique(dat$state)
+fig_RE2<-ggplot() +
+  geom_pointrange(data = dat_RE2, aes(x = state, y = mean, 
+                                      ymin = pc2.5, ymax = pc97.5), 
+                  size = 0.25) +
+  geom_hline(yintercept = 0, col = "red") +
+  scale_y_continuous("RE (area, 1e5 acres)")+ 
+  scale_x_discrete() +
+  theme_bw(base_size = 12)+
+  theme(panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.title.y = element_blank()) +
+  coord_flip()
+print(fig_RE2)
+
+dat_RE3 <- sum_out[grep("Estar\\[[0-9]{1,2}\\,3", row.names(sum_out)),]
+dat_RE3$state <- unique(dat$state)
+fig_RE3<-ggplot() +
+  geom_pointrange(data = dat_RE3, aes(x = state, y = mean, 
+                                      ymin = pc2.5, ymax = pc97.5), 
+                  size = 0.25) +
+  geom_hline(yintercept = 0, col = "red") +
+  scale_y_continuous("RE (sales, 1e6 $)")+ 
+  scale_x_discrete() +
+  theme_bw(base_size = 12)+
+  theme(panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.title.y = element_blank()) +
+  coord_flip()
+print(fig_RE3)
+
+jpeg(filename = "figs/fig_RE.jpg", height = 6, width = 8, units = "in", 
+     res = 600)
+plot_grid(fig_RE1, fig_RE2, fig_RE3, ncol = 3)
+dev.off()
+
+### Standard deviation
+dat_sigs <- sum_out[grep("Sig\\[", row.names(sum_out)),]
+fig_sig <- ggplot(dat_sigs, aes(x = var)) +
+  geom_pointrange(aes(y = mean, ymin = pc2.5, ymax = pc97.5), size = 0.5) +
+  scale_y_continuous(expression(paste(sigma))) + 
+  scale_x_discrete(labels = labs) +
+  theme_bw(base_size=12) +
+  theme(panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.title.y = element_blank()) +
+  coord_flip()
+print(fig_sig)
+
+#Rho, or correlation coefficient between each pair of traits
+dat_rho <- sum_out[grep("Rho\\[", row.names(sum_out)),]
+dat_rho$labs <- c("number - area", "number - sales", "area - sales")
+fig_rho <- ggplot(dat_rho, aes(x = labs)) +
+  geom_pointrange(aes(y = mean, ymin = pc2.5, ymax = pc97.5), size = 0.5) +
+  scale_y_continuous(expression(paste(rho))) + 
+  scale_x_discrete() +
+  theme_bw(base_size=12) +
+  theme(panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.title.y = element_blank()) +
+  coord_flip()
+print(fig_rho)
+
+jpeg(filename = "figs/fig_sig_rho.jpg", height = 4, width = 8, units = "in", 
+     res = 600)
+plot_grid(fig_sig, fig_rho, ncol = 2)
+dev.off()
+
+# Replicated data
+# Compile and adapt BUGS model
+start <- proc.time()
+model_rep <- bugs(data = datlist, 
+              inits = saved.state[[2]],
+              parameters.to.save = "farm.rep", 
+              n.iter = 10000, n.chains = 3, n.burnin = 5000, n.thin = 10,
+              model.file="mod_1a.R", 
+              codaPkg = TRUE, debug = FALSE)
+end <- proc.time()
+print((end - start)/60)
+
+#change to coda object if codaPkg=T
+coda_rep <- read.bugs(model_rep)
+save(coda_rep, file = "coda/coda_rep_1a.Rdata")
+
+#check convergence
+gel <- gelman.diag(coda_rep, multivariate = F)
+mcmcplot(coda_rep)
+
+#summarizing chains, reshape, append to data
+sum_rep <- coda.fast(chains = 3, burn.in = 0, thin = 1, coda = coda_rep)
+
+pred_df <- data.frame(pivot_longer(dat,3:5, names_to = "type", values_to = "obs"),
+                      sum_rep[grep("farm.rep", row.names(sum_rep)),]) %>%
+  mutate(Type = case_when(type == "farm_5ha" ~ "Area (1e5 acres)",
+                          type == "farm_knumber" ~ "Number (1e3)",
+                          type == "farm_msales" ~ "Sales (1e6 $)"),
+         Type = factor(Type, levels = labs),
+         coverage = ifelse(obs <= pc97.5 & obs >= pc2.5, 1, 0))
+tapply(pred_df$coverage, pred_df$Type, mean,  na.rm = T)
+
+fitByType <- by(
+  data    = pred_df,
+  INDICES = pred_df$Type,
+  FUN     = function(subset)
+    summary(lm(mean ~ obs, data = subset)))
+
+bestfit <- data.frame(Type = labs,
+                      R2 = c(fitByType[[1]]$adj.r.squared, 
+                             fitByType[[2]]$adj.r.squared,
+                             fitByType[[3]]$adj.r.squared),
+                      slope = c(fitByType[[1]]$coef[2,1],
+                                fitByType[[2]]$coef[2,1],
+                                fitByType[[3]]$coef[2,1]),
+                      int = c(fitByType[[1]]$coef[1,1],
+                              fitByType[[2]]$coef[1,1],
+                              fitByType[[3]]$coef[1,1])) %>%
+  mutate(r2 = paste0("R^2==", round(R2, 3)),
+         Type = factor(Type, levels = labs))
+
+fig_fit <- ggplot() +
+  geom_abline(slope = 1, intercept = 0, col = "red") +
+  geom_abline(data = bestfit, aes(slope = slope, intercept = int), col = "gray", lty = 2) +
+  geom_errorbar(data = pred_df, aes(x = obs, ymin = pc2.5, ymax = pc97.5), color = "gray", width = 0, alpha = 0.5) +
+  geom_point(data = pred_df, aes(x = obs, y = mean), alpha = 0.75) +
+  geom_text(data = bestfit, aes(x = 2.5, y = 0, label = r2), parse = TRUE,
+            hjust = 0, vjust = 1) +
+  scale_x_continuous("Observed") +
+  scale_y_continuous("Predicted") +
+  facet_wrap(~Type) +
+  theme_bw(base_size = 12) +
+  theme(panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        strip.background = element_blank()) +
+  coord_equal()
+jpeg(filename = "figs/fig_fit.jpg", height = 3, width = 7, units = "in",
+     res = 600)
+print(fig_fit)
+dev.off()
+                  
