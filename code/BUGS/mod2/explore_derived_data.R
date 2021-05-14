cda0c29d0e65ace692208d74337a74910149f6f7
Jessica Guo <jessicaguo@github.com>
2021-05-14 10:41:22 -0700

    Further explore lack of signficant trend in farm number over time

--- code/BUGS/mod2/explore_derived_data.R
+++ code/BUGS/mod2/explore_derived_data.R
@@ -9,15 +9,59 @@ ggplot(dat, aes(x = year)) +
   geom_point(aes(y = value, color = state)) +
   facet_wrap(~variable, scales = "free_y") +
   scale_y_continuous(limits = c(0, 100)) +
-  guides(color = F)
+  guides(color = FALSE)
 
 # Plot on log scale
 ggplot(dat, aes(x = year)) +
   geom_point(aes(y = log(value), color = state)) +
   facet_wrap(~variable, scales = "free_y") +
-  guides(color = F)
+  guides(color = FALSE)
 
 tapply(dat$value, dat$variable, FUN = min, na.rm = TRUE)
 
 # States clearly have different slopes when on linear scale, 
 # but differences disappear on log scale
+
+# Plot just farm knumber on both scales
+farm <- dat %>%
+  filter(variable == "farm_knumber") %>%
+  mutate(lvalue = log(value))
+
+ggplot(farm, aes(x = year, color = state, group = state)) +
+  geom_point(aes(y = lvalue)) +
+  geom_line(aes(y = lvalue)) +
+  scale_y_continuous("log farm number") +
+  scale_x_continuous(breaks = unique(farm$year)) +
+  theme_bw(base_size = 12) +
+  guides(color = FALSE)
+
+ggplot(farm, aes(x = year, color = state, group = state)) +
+  geom_point(aes(y = value)) +
+  geom_line(aes(y = value)) +
+  scale_y_continuous("farm number", limits = c(0, 0.5)) +
+  scale_x_continuous(breaks = unique(farm$year)) +
+  theme_bw(base_size = 12) +
+  guides(color = FALSE)
+
+
+# Plot just farm number on both scales
+farm2 <- dat %>%
+  filter(variable == "farm_knumber") %>%
+  mutate(value = value*1000,
+         lvalue = log(value))
+
+ggplot(farm2, aes(x = year, color = state, group = state)) +
+  geom_point(aes(y = lvalue)) +
+  geom_line(aes(y = lvalue)) +
+  scale_y_continuous("log farm number") +
+  scale_x_continuous(breaks = unique(farm$year)) +
+  theme_bw(base_size = 12) +
+  guides(color = FALSE)
+
+ggplot(farm2, aes(x = year, color = state, group = state)) +
+  geom_point(aes(y = value)) +
+  geom_line(aes(y = value)) +
+  scale_y_continuous("farm number") +
+  scale_x_continuous(breaks = unique(farm$year)) +
+  theme_bw(base_size = 12) +
+  guides(color = FALSE)
